a5efb7c0c0b0300ece8e225e15e3d1522a37cd1f
Izumemori <me@izu.re>
2020-01-10 17:57:36 +0100

    move to sub dir

---
+++ keyboards/massdrop/ctrl/keymaps/izu/keymap.c
@@ -0,0 +1,236 @@
+#include QMK_KEYBOARD_H
+#define TG_NKRO MAGIC_TOGGLE_NKRO
+
+enum ctrl_keycodes {
+    U_T_AUTO = SAFE_RANGE, //USB Extra Port Toggle Auto Detect / Always Active
+    U_T_AGCR,              //USB Toggle Automatic GCR control
+    DBG_TOG,               //DEBUG Toggle On / Off
+    DBG_MTRX,              //DEBUG Toggle Matrix Prints
+    DBG_KBD,               //DEBUG Toggle Keyboard Prints
+    DBG_MOU,               //DEBUG Toggle Mouse Prints
+    MD_BOOT,               //Restart into bootloader after hold timeout
+    MC_TRINIT_SUCKS
+};
+
+keymap_config_t keymap_config;
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    [0] = LAYOUT(
+        KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,             KC_PSCR, KC_SLCK, KC_PAUS, \
+        KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC,   KC_INS,  KC_HOME, KC_PGUP, \
+        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,   KC_DEL,  KC_END,  KC_PGDN, \
+        MO(2), KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT, \
+        KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,                              KC_UP, \
+        KC_LCTL, KC_LGUI, KC_LALT,                   KC_SPC,                             KC_RALT, MO(1),   KC_APP,  KC_RCTL,            KC_LEFT, KC_DOWN, KC_RGHT \
+    ),
+    [1] = LAYOUT(
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,            KC_MUTE, _______, _______, \
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,   KC_MPLY, KC_MSTP, KC_VOLU, \
+        _______, RGB_SPD, RGB_VAI, RGB_SPI, RGB_HUI, RGB_SAI, _______, _______, _______, _______, _______, _______, _______, _______,   KC_MPRV, KC_MNXT, KC_VOLD, \
+        _______, RGB_RMOD,RGB_VAD, RGB_MOD, RGB_HUD, RGB_SAD, _______, _______, _______, _______, _______, _______, _______, \
+        _______, RGB_TOG, _______, _______, _______, MD_BOOT, TG_NKRO, DBG_TOG, _______, _______, _______, _______,                              _______, \
+        _______, _______, _______,                   _______,                            _______, _______, _______, _______,            _______, _______, _______ \
+    ),
+    [2] = LAYOUT(
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,            _______, _______, _______, \
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,   _______, _______, _______, \
+        _______, MC_TRINIT_SUCKS, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,   _______, _______, _______, \
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,                              _______, \
+        _______, _______, _______,                   _______,                            _______, _______, _______, _______,            _______, _______, _______ \
+    ),
+};
+
+uint8_t KEYCODE_TO_LED_ID[256];
+
+static void init_keycode_to_led_map(void){
+    uint16_t LED_MAP[MATRIX_ROWS][MATRIX_COLS] = LAYOUT(
+            0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,
+            20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,
+            36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,
+            52,53,54,55,56,57,58,59,60,61,62,63,64,65,66,67,
+            68,69,70,71,72,73,74,75,76,77,78,79,80,81,82,83,84,85,86
+    );
+
+    uint16_t key = 0;
+    for(uint8_t y = 0; y < MATRIX_ROWS; ++y){
+        for(uint8_t x = 0; x < MATRIX_COLS; ++x){
+            key = keymaps[0][y][x];
+            if(key < 256){
+                KEYCODE_TO_LED_ID[key] = LED_MAP[y][x];
+            }
+        }
+    }
+}
+
+void rgb_matrix_indicators_user(void) {
+    if (!g_suspend_state && rgb_matrix_config.enable) {
+        switch (biton32(layer_state)) {
+            case 1:
+
+                for (int i = 0; i < MATRIX_ROWS; i++) {
+                    for (int j = 0; j < MATRIX_COLS; j++) {
+
+                        uint16_t key1 = keymaps[0][i][j];
+                        uint16_t key2 = keymaps[1][i][j];
+
+                        uint16_t led = KEYCODE_TO_LED_ID[key1];
+
+                        if (led < 13) continue;
+
+                        switch (key2)
+                        {
+                            case _______:
+                                rgb_matrix_set_color(led, 0, 0, 0);
+                            break;
+
+                            case TG_NKRO:
+                                if (keymap_config.nkro) rgb_matrix_set_color(led, 0, 255, 0);
+                                else rgb_matrix_set_color(led, 255, 0, 0);
+                            break;
+
+                            default:
+                                rgb_matrix_set_color(led, 255, 255, 255);
+                            break;
+                        }
+
+                        switch (led)
+                        {
+                            case 36:
+                                rgb_matrix_set_color(led, 255, 255, 255);
+                            break;
+                        }
+                    }
+                }
+
+            break;
+
+            case 2:
+                for (int i = 0; i < MATRIX_ROWS; i++) {
+                    for (int j = 0; j < MATRIX_COLS; j++) {
+
+                        uint16_t key1 = keymaps[0][i][j];
+                        uint16_t key2 = keymaps[2][i][j];
+
+                        uint16_t led = KEYCODE_TO_LED_ID[key1];
+
+                        if (led < 13) continue;
+
+                        switch (key2)
+                        {
+                            case _______:
+                                rgb_matrix_set_color(led, 0, 0, 0);
+                            break;
+
+                            case MC_TRINIT_SUCKS:
+                                rgb_matrix_set_color(led, 125, 67, 102);
+                            break;
+
+                            default:
+                                rgb_matrix_set_color(led, 255, 255, 255);
+                            break;
+                        }
+                    }
+                }
+            break;
+        }
+    }
+};
+
+// Runs just one time when the keyboard initializes.
+void matrix_init_user(void) {
+    init_keycode_to_led_map();
+};
+
+// Runs constantly in the background, in a loop.
+void matrix_scan_user(void) {
+};
+
+#define MODS_SHIFT  (get_mods() & MOD_BIT(KC_LSHIFT) || get_mods() & MOD_BIT(KC_RSHIFT))
+#define MODS_CTRL  (get_mods() & MOD_BIT(KC_LCTL) || get_mods() & MOD_BIT(KC_RCTRL))
+#define MODS_ALT  (get_mods() & MOD_BIT(KC_LALT) || get_mods() & MOD_BIT(KC_RALT))
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    static uint32_t key_timer;
+
+    switch (keycode) {
+        case MC_TRINIT_SUCKS:
+            if (record->event.pressed)
+            {
+                SEND_STRING("trinit sucks :^)");
+            }
+            return false;
+        case U_T_AUTO:
+            if (record->event.pressed && MODS_SHIFT && MODS_CTRL) {
+                TOGGLE_FLAG_AND_PRINT(usb_extra_manual, "USB extra port manual mode");
+            }
+            return false;
+        case U_T_AGCR:
+            if (record->event.pressed && MODS_SHIFT && MODS_CTRL) {
+                TOGGLE_FLAG_AND_PRINT(usb_gcr_auto, "USB GCR auto mode");
+            }
+                return false;
+        case DBG_TOG:
+            if (record->event.pressed) {
+                TOGGLE_FLAG_AND_PRINT(debug_enable, "Debug mode");
+            }
+            return false;
+        case DBG_MTRX:
+            if (record->event.pressed) {
+                TOGGLE_FLAG_AND_PRINT(debug_matrix, "Debug matrix");
+            }
+            return false;
+        case DBG_KBD:
+            if (record->event.pressed) {
+                TOGGLE_FLAG_AND_PRINT(debug_keyboard, "Debug keyboard");
+            }
+            return false;
+        case DBG_MOU:
+            if (record->event.pressed) {
+                TOGGLE_FLAG_AND_PRINT(debug_mouse, "Debug mouse");
+            }
+            return false;
+        case TG_NKRO:
+            if (record->event.pressed) {
+                keymap_config.nkro = !keymap_config.nkro;
+            }
+            return true;
+        case MD_BOOT:
+            if (record->event.pressed) {
+                key_timer = timer_read32();
+            } else {
+                if (timer_elapsed32(key_timer) >= 500) {
+                    reset_keyboard();
+                }
+            }
+            return false;
+        case RGB_TOG:
+            if (record->event.pressed) {
+              switch (rgb_matrix_get_flags()) {
+                case LED_FLAG_ALL: {
+                    rgb_matrix_set_flags(LED_FLAG_KEYLIGHT | LED_FLAG_MODIFIER);
+                    rgb_matrix_set_color_all(0, 0, 0);
+                  }
+                  break;
+                case LED_FLAG_KEYLIGHT | LED_FLAG_MODIFIER: {
+                    rgb_matrix_set_flags(LED_FLAG_UNDERGLOW);
+                    rgb_matrix_set_color_all(0, 0, 0);
+                  }
+                  break;
+                case LED_FLAG_UNDERGLOW: {
+                    rgb_matrix_set_flags(LED_FLAG_NONE);
+                    rgb_matrix_disable_noeeprom();
+                  }
+                  break;
+                default: {
+                    rgb_matrix_set_flags(LED_FLAG_ALL);
+                    rgb_matrix_enable_noeeprom();
+                  }
+                  break;
+              }
+            }
+            return false;
+        default:
+            return true; //Process all other keycodes normally
+    }
+}
